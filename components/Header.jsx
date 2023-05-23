e5b631324edb50666dbb788491de297afe272112
leviathan <stevenvelez1368@gmail.com>
2023-05-22 21:28:14 -0400

    Created a header and footer (to be updated to look more in style) showed this in the login page)

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="flex gap-4 bg-stone-600">
+      <p className="text-size-3xl text-white"> Header </p>
+    </nav>
+  );
+};
+
+export default Header;
