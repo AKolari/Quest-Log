4194cdacd20b6d6b8810ddb4b92eac8da371a222
Andi Kolari <AndiKolari627@gmail.com>
2023-05-22 23:01:47 -0400

    resolved conflict

--- components/Quest.jsx
+++ components/Quest.jsx
@@ -1,9 +1,24 @@
 const Quest = ({ quest_name, quest_description, editable }) => {
-  return (
-    <div>
-      <h1>{quest_name}</h1>
-      <p>{quest_description}</p>
-    </div>
-  ); //Hello this is steven!
+  if (!editable) {
+    return (
+      <div>
+        <h1>{quest_name}</h1>
+        <p>{quest_description}</p>
+      </div>
+    ); //Hello this is steven!
+    //testing new branch
+  } else {
+    return (
+      <div>
+        <input type="text" id="quest_name" value={quest_name}></input>
+        <input
+          type="text"
+          id="quest_description"
+          value={quest_description}
+          onChange={() => {}}
+        ></input>
+      </div>
+    );
+  }
 };
 export default Quest;
