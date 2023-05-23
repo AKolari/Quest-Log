ff61bddd3066fe9cee930626118c59cc1a8b948f
AKolari <90071560+AKolari@users.noreply.github.com>
2023-05-23 00:04:08 -0400

    Update Quest.jsx

--- components/Quest.jsx
+++ components/Quest.jsx
@@ -9,12 +9,18 @@ const Quest = ({ quest_name, quest_description, editable }) => {
   } else {
     return (
       <div>
-        <input type="text" id="quest_name" value={quest_name}></input>
+        <input
+          type="text"
+          id="quest_name"
+          value={quest_name}
+          className="h3 border-2 border-double border-stone-400 bg-amber-600 border-black ml-5 inline-block w-[220px] px-2"
+        ></input>
         <input
           type="text"
           id="quest_description"
           value={quest_description}
           onChange={() => {}}
+          className="h3 border-2 border-double border-stone-400 bg-amber-600 border-black ml-5 inline-block w-[220px] px-2"
         ></input>
       </div>
     );
