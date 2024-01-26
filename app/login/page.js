751d5d00fc1b7f8e43ffa6786a4e4482b8ea4832
AKolari <90071560+AKolari@users.noreply.github.com>
2024-01-26 16:47:25 -0500

    fixed deployment bug

--- app/login/page.js
+++ app/login/page.js
@@ -4,8 +4,14 @@ import Login from "@/components/Login";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 function Page() {
+
   const router = useRouter();
-  router.push('/');
+  useEffect(()=>{
+    
+    router.push('/');
+  }, [router])
+ 
 }
 export default Page;
