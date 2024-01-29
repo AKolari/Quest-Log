88c34c504a949ea20d2bc03098cdc502b0adf932
AKolari <90071560+AKolari@users.noreply.github.com>
2024-01-29 16:55:07 -0500

    Fixed deployment bug

--- app/register/page.js
+++ app/register/page.js
@@ -1,6 +1,6 @@
 "use client"
 import Register from "@/components/Register";
-import useRouter from "next/navigation";
+import {useRouter} from "next/navigation";
 
 function Page() {
   const router = useRouter();
